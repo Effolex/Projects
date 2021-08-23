2f3142c88cb92ce2d72dfd4b7801cec5e1528acd
Effolex <32694502+Effolex@users.noreply.github.com>
2021-08-23 03:42:06 -0300

    Update script.js

--- personal-projects/clock/script.js
+++ personal-projects/clock/script.js
@@ -3,7 +3,7 @@ const days = document.querySelector('#days');
 const hours = document.querySelector('#hours');
 const minutes = document.querySelector('#minutes');
 const seconds = document.querySelector('#seconds');
-const date = new Date('12/25/2022');
+const date = new Date('08/25/2022');
 
 function formatTime(time){
     const timeFormated = Math.floor(time);
